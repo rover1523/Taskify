f9354dfd6535d530492f94b041b2d417eaaec17b
gyoyeon Kim <gy2121@naver.com>
2025-03-19 13:12:01 +0900

    Feat: InviteRecords > 이메일,취소 버튼 추가

--- src/components/commoon/table/InviteRecords.tsx
+++ src/components/commoon/table/InviteRecords.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import Pagination from "../Pagination";
 
-const InviteList = () => {
+const InviteRecords = () => {
   const [invitelog, setInvitelog] = useState([
     // 나중에 api 데이터로 변경
     { email: "codeit1@codeit.com" },
@@ -16,7 +16,7 @@ const InviteList = () => {
   const itemsPerPage = 5;
   const totalPages = Math.ceil(invitelog.length / itemsPerPage);
 
-  const paginatedMembers = invitelog.slice(
+  const paginatedInvitation = invitelog.slice(
     (currentPage - 1) * itemsPerPage,
     currentPage * itemsPerPage
   );
@@ -34,5 +34,43 @@ const InviteList = () => {
     if (currentPage < totalPages) setCurrentPage(currentPage + 1);
   };
 
-  return <div></div>;
+  return (
+    <div className="relative bg-white p-6 rounded-lg shadow-md max-w-md w-full sm:max-w-lg md:max-w-xl lg:max-w-2xl mx-auto">
+      <div className="flex justify-between items-center">
+        <p className="text-lg sm:text-xl font-bold">초대 내역</p>
+        <Pagination
+          currentPage={currentPage}
+          totalPages={totalPages}
+          onPrev={handlePrevPage}
+          onNext={handleNextPage}
+        />
+      </div>
+      {/* 구성원 리스트 */}
+      <p className="text-sm sm:text-base text-gray-500 mt-6">이메일</p>
+      <ul>
+        {paginatedInvitation.map((invitelog, index) => (
+          <li
+            key={index}
+            className={`flex items-center justify-between p-4 ${
+              index !== paginatedInvitation.length - 1
+                ? "border-b border-gray-200"
+                : ""
+            }`}
+          >
+            <div className="flex items-center gap-4">
+              <p className="text-sm sm:text-base">{invitelog.email}</p>
+            </div>
+            <button
+              onClick={() => handleDelete(invitelog.email)}
+              className="cursor-pointer font-medium text-sm sm:text-base h-[32px] sm:h-[32px] w-[52px] sm:w-[84px] md:w-[84px] border border-gray-300 text-indigo-600 px-2 py-1 rounded-md hover:bg-gray-100"
+            >
+              취소
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
+
+export default InviteRecords;
--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,12 +1,16 @@
 // 컴포넌트 test 파일
 
 import MemberList from "@/components/commoon/table/MemberList";
+import InviteRecords from "@/components/commoon/table/InviteRecords";
 
 export default function TestPage() {
   return (
     <div className="flex flex-col items-center gap-4 p-10">
       <h1 className="text-2xl font-bold">Profile 테스트</h1>
       <MemberList />
+      <br />
+      <h1 className="text-2xl font-bold">초대 테이블 테스트</h1>
+      <InviteRecords />
     </div>
   );
 }
