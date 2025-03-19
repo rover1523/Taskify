6b3885c3937766683a0eeb882958e55b715561c3
gyoyeon Kim <gy2121@naver.com>
2025-03-20 00:38:46 +0900

    feat: 초대받은 대시보드 반응형

--- src/components/commoon/table/InvitedDashBoard.tsx
+++ src/components/commoon/table/InvitedDashBoard.tsx
@@ -3,18 +3,18 @@ import Image from "next/image";
 
 function EmptyInvitations() {
   return (
-    <div className="relative w-[960px] h-[390px] top-[20px] left-[20px] rounded-[16px] p-[24px_40px_120px_40px] gap-[64px] bg-white shadow-md mx-auto">
-      <div className="flex justify-between items-center">
-        <p className="text-xl sm:text-2xl font-bold mb-4">초대받은 대시보드</p>
+    <div className="relative sm:w-[960px] w-[260px] sm:h-[390px] h:[327px]  rounded-[16px] sm:p-[24px_40px_120px_40px] p-[24px_20px_80px_20px]  bg-white shadow-md  mx-auto">
+      <div className="flex justify-between ">
+        <p className="sm:text-2xl text-sm font-bold mb-4 ">초대받은 대시보드</p>
       </div>
 
-      <div className="flex flex-col justify-center items-center h-[calc(100%-40px)]">
+      <div className="mb-[16px] flex flex-col justify-center items-center h-[calc(100%-40px)]">
         <img
-          className="w-[100px] h-[100px] mb-2"
+          className="sm:w-[100px] sm:h-[100px] w:-[60px] h:-[60px] mb-2"
           src="/svgs/unsubscribe.svg"
           alt="초대받은 대시보드가 없을 때 아이콘 이미지"
         />
-        <p className="text-lg leading-[26px] text-[#9FA6B2]">
+        <p className="sm:text-lg text-xs leading-[26px] text-[#9FA6B2] whitespace-nowrap">
           아직 초대받은 대시보드가 없어요
         </p>
       </div>
