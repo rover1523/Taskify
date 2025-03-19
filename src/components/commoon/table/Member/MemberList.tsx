93db42406d00c1dd723f27e09b347a97029cc16e
gyoyeon Kim <gy2121@naver.com>
2025-03-19 22:50:04 +0900

    rename: Meber 폴더 생성

--- src/components/commoon/table/Member/MemberList.tsx
+++ src/components/commoon/table/Member/MemberList.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Pagination from "../Pagination";
+import Pagination from "../../Pagination";
 import RandomProfile from "./RandomProfile";
 
 const MemberList = () => {
