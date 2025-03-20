486f18bd48f43c216631cc89d656d4a1d0eca2bf
gyoyeon Kim <gy2121@naver.com>
2025-03-20 14:55:28 +0900

    [Feat] global.css 코드 추가

--- src/components/commoon/table/InvitedDashBoard.tsx
+++ src/components/commoon/table/InvitedDashBoard.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, ChangeEvent, useEffect } from "react";
 import Image from "next/image";
 
 function EmptyInvitations() {
@@ -12,7 +12,7 @@ function EmptyInvitations() {
         <img
           className="sm:w-[100px] sm:h-[100px] w:-[60px] h:-[60px] mb-2"
           src="/svgs/unsubscribe.svg"
-          alt="초대받은 대시보드가 없을 때 아이콘 이미지"
+          alt="대시보드 없을때 아이콘"
         />
         <p className="sm:text-lg text-xs leading-[26px] text-[#9FA6B2] whitespace-nowrap">
           아직 초대받은 대시보드가 없어요
@@ -24,18 +24,51 @@ function EmptyInvitations() {
 
 function InvitedList() {
   return (
-    <div className="relative sm:p-[32px_0px_0px_32px]  bg-white p-6 rounded-lg shadow-md w-[1022px] h-[650px] max-w-none mx-auto">
-      <div className="flex justify-between items-center">
-        <p className="text-xl sm:text-2xl font-bold">초대받은 대시보드</p>
+    <div className="relative bg-white w-[1022px] h-[458px]  mx-auto mt-[24px]">
+      <div className="flex w-[798px] h-[26px] justify-between items-center pl-[76px] pr-[32px]">
+        <p className="font-normal text-[#9FA6B2]">이름</p>
+        <p className="font-normal text-[#9FA6B2]">초대자</p>
+        <p className="font-normal text-[#9FA6B2]">수락여부</p>
       </div>
     </div>
   );
 }
 
 export default function InvitedDashBoard() {
+  const [searchTitle, setSearchTitle] = useState("");
+
+  /* 검색창 input */
+  const handleSearchInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setSearchTitle(event.target.value);
+    // 추후 삭제
+    console.log("search input 값: ", event.target.value);
+  };
+
   return (
     <div>
-      <EmptyInvitations /> <br /> <InvitedList />{" "}
+      <div className="relative   bg-white p-6 rounded-lg shadow-md w-[1022px] h-[650px] max-w-none mx-auto">
+        <div className="relative w-[966px] h-[104px]">
+          <div className="flex justify-between items-center mb-[32px]">
+            <p className="text-xl sm:text-2xl font-bold">초대받은 대시보드</p>
+          </div>
+          <div className="p-[6px 16px 6px 16px] relative w-[966px]">
+            <input
+              id="title"
+              placeholder="검색"
+              type="text"
+              value={searchTitle}
+              onChange={handleSearchInputChange}
+              className="text-[#9FA6B2] w-full h-[40px] px-[40px] py-[6px] border border-[#D9D9D9] bg-white rounded-[6px]  placeholder-gray-400 outline-none"
+            />
+            <img
+              src="/svgs/search.svg"
+              alt="검색 아이콘"
+              className="absolute left-[12px] top-1/2 transform -translate-y-1/2 w-[20px] h-[20px]"
+            />
+          </div>
+          <InvitedList />
+        </div>
+      </div>
     </div>
   );
 }
