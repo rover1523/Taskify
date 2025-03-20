05c78d7a570672be4997d9566987b0cbb25cb36a
hyejin <hhyejin90@gmail.com>
2025-03-20 21:07:55 +0900

    [Fix] TypeScript undefined 타입 오류 해결

--- src/components/Button/PaginationBtn.tsx
+++ src/components/Button/PaginationBtn.tsx
@@ -1,4 +1,12 @@
-export const PaginationBtn = ({
+import React from "react";
+
+interface PaginationBtnProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  direction?: "left" | "right";
+  disabled?: boolean;
+}
+
+export const PaginationBtn: React.FC<PaginationBtnProps> = ({
   direction = "left",
   disabled = false,
   ...props
@@ -7,7 +15,7 @@ export const PaginationBtn = ({
     "w-[40px] h-[40px] flex justify-center items-center border border-[#D9D9D9] rounded-md text-[16px] font-medium cursor-pointer transition";
 
   const enabledTextColor = "text-[#787486] hover:bg-gray-100";
-  const disabledTextColor = "text-[#D9D9D9] cursor-not-allowed";
+  const disabledTextColor = "text-[#D9D9D9] ";
 
   const finalStyle = `${baseStyle} ${disabled ? disabledTextColor : enabledTextColor}`;
 
