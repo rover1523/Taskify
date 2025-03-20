4feceddab59812c933dd37c26493b48ad355386c
MinJi Cho <jam96013@gmail.com>
2025-03-21 01:22:59 +0900

    [Feat] 기본 헤더 반응형 디자인 추가 & _app.tsx파일에 실험  코드 작성

--- src/components/Gnb/Header_default.tsx
+++ src/components/Gnb/Header_default.tsx
@@ -2,16 +2,21 @@ import React from "react";
 
 const HeaderDefault = () => {
   return (
-    <header className="w-full h-[70px] flex items-center justify-center bg-white border-b-[1px] border-b-[#D9D9D9]">
-      <div className="w-[1780px] h-[39px] flex items-center justify-between">
-        <div className="flex items-center space-x-2 cursor-pointer">
+    <header className="w-full h-[70px] md:h-[70px] sm:h-[60px] flex items-center justify-center bg-white border-b-[1px] border-b-[#D9D9D9]">
+      <div className="w-full flex items-center justify-between lg:px-[70px] md:px-[40px] sm:px-[24px]">
+        <div className="flex items-center space-x-2 cursor-pointer relative">
           <img
             src="../svgs/large-logo.svg"
             alt="Taskify Logo"
-            className="h-[39px]"
+            className="h-[39px] hidden md:block"
+          />
+          <img
+            src="../svgs/small-logo.svg"
+            alt="Taskify Mobile Logo"
+            className="h-[27px] block md:hidden"
           />
         </div>
-        <div className="flex space-x-4 text-gray600">
+        <div className="flex space-x-4">
           <button className="font-16r text-black3">로그인</button>
           <button className="font-16r text-black3">회원가입</button>
         </div>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import "@/components/Gnb/Header_default";
 import HeaderDefault from "@/components/Gnb/Header_default";
 
 export default function App({ Component, pageProps }: AppProps) {
