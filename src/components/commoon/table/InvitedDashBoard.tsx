4d5adfc93033c4823b1530e0590b27599cf91b27
gyoyeon Kim <gy2121@naver.com>
2025-03-20 22:08:44 +0900

    [Fix] 초대받은 대시보드 웹 ui 글자 위치 수정

--- src/components/commoon/table/InvitedDashBoard.tsx
+++ src/components/commoon/table/InvitedDashBoard.tsx
@@ -18,16 +18,17 @@ const invitedData = [
 /* 초대 받은 대시보드가 없을 경우 */
 function EmptyInvitations() {
   return (
-    <div className="relative sm:w-[960px] w-[260px] sm:h-[390px] h:[327px] rounded-[16px] sm:p-[24px_40px_120px_40px] p-[24px_20px_80px_20px] bg-white shadow-md mx-auto">
-      <div className="mb-[16px] flex flex-col justify-center items-center h-[calc(100%-40px)]">
-        <Image
-          src="/svgs/unsubscribe.svg"
-          alt="초대받은 대시보드 없을때 아이콘"
-          width={60}
-          height={60}
-          className="sm:w-[100px] sm:h-[100px] w-[60px] h-[60px] mb-2"
-        />
+    <div className="relative sm:w-[960px] w-[260px] sm:h-[390px] h:[327px]  rounded-[16px] sm:p-[24px_40px_120px_40px] p-[24px_20px_80px_20px]  bg-white shadow-md  mx-auto">
+      <div className="flex justify-between ">
+        <p className="sm:text-2xl text-sm font-bold mb-4 ">초대받은 대시보드</p>
+      </div>
 
+      <div className="mb-[16px] flex flex-col justify-center items-center h-[calc(100%-40px)]">
+        <img
+          className="sm:w-[100px] sm:h-[100px] w:-[60px] h:-[60px] mb-2"
+          src="/svgs/unsubscribe.svg"
+          alt="대시보드 없을때 아이콘"
+        />
         <p className="sm:text-lg text-xs leading-[26px] text-[var(--color-gray2)] whitespace-nowrap">
           아직 초대받은 대시보드가 없어요
         </p>
@@ -108,6 +109,17 @@ export default function InvitedDashBoard() {
     setSearchTitle(event.target.value);
     console.log("search input 값: ", event.target.value);
   };
+
+  /* 검색된 데이터 필터링 */
+  const filteredData = invitedData.filter(
+    (invite) =>
+      invite.title.toLowerCase().includes(searchTitle.toLowerCase()) ||
+      invite.nickname.toLowerCase().includes(searchTitle.toLowerCase())
+  );
+
+  if (invitedData.length === 0) {
+    return <EmptyInvitations />;
+  }
 
   return (
     <div>
