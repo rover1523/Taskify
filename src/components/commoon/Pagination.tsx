571de5103cc403c955e9617657e1a5efb2ceff78
gyoyeon Kim <gy2121@naver.com>
2025-03-21 14:38:12 +0900

    [Refactor] Pagination.tsx 페이지 안내 p 태그 수정

--- src/components/commoon/Pagination.tsx
+++ src/components/commoon/Pagination.tsx
@@ -16,7 +16,7 @@ const Pagination: React.FC<PaginationProps> = ({
   return (
     <div className="flex items-center">
       <p className="relative -translate-y-[10px] sm:text-sm text-xs text-gray-700 mt-4 text-center mr-3">
-        {currentPage} 페이지 중 {totalPages}
+        {totalPages} 페이지 중 {currentPage}
       </p>
       <div className="flex items-center border border-gray-300 rounded-lg overflow-hidden">
         <button
