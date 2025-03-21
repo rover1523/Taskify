d41f391c559b390d45bd8df114d3d7286cfc6e0c
rover1523 <wdd152323@gmail.com>
2025-03-21 15:08:34 +0900

    [Docs] 문서수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,6 @@
-import CardButton from "./components/button/CardButton";
-import ColumnsButton from "./components/button/ColumnsButton";
-import DashboardAddButton from "./components/button/DashboardAddButton";
-import DashboardDeleteButton from "./components/button/DashboardDeleteButton";
-import TodoButton from "./components/button/TodoButton";
+import React, { useState } from "react";
+import Head from "next/head";
 
-export default function Home() {
-  return (
-    <div className="flex flex-col items-center p-10 ">
-      <h1 className="text-xl font-semibold text-white mb-6">반응형 버튼</h1>
-
-      {/* 컬럼 추가 버튼 */}
-      <ColumnsButton>새로운 컬럼 추가하기</ColumnsButton>
-      <DashboardAddButton />
-      <DashboardDeleteButton />
-      <TodoButton />
-      <CardButton />
-    </div>
-  );
+export default function Document() {
+  return <div>Hello</div>;
 }
