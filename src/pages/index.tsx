9f21a68bcc0f031435f83a59bf9df28fb60fbfd0
hyejin <hhyejin90@gmail.com>
2025-03-21 23:21:18 +0900

    [Chore]console 삭제

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import Head from "next/head";
-import { CustomButton } from "../components/Button/Button";
 
 export default function Document() {
   return <div>Hello</div>;
