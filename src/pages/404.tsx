0addedb6a7f4741833a481b02426ac8cc0a748eb
MinJi Cho <jam96013@gmail.com>
2025-03-25 14:04:08 +0900

    [Feat] 타임아웃 시 홈페이지 이동 기능 추가

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,10 +1,18 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
 export default function Custom404() {
+  const router = useRouter();
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.push("/");
+    }, 10000);
+  });
+
   return (
     <div className="flex flex-col items-center justify-center h-screen text-center">
-      <h1 className="text-4xl font-bold text-[var(--primary)]">
-        404 Not Found
-      </h1>
-      <p className="mt-2 text-lg text-black3">페이지를 찾을 수 없습니다.</p>
+      <h1 className="font-24b text-[var(--primary)]">404 Not Found</h1>
+      <p className="mt-1 font-16r text-black3">페이지를 찾을 수 없습니다.</p>
     </div>
   );
 }
