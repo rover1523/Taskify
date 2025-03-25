9c224bf962f2140e18290a0b93cfb6ccea8f09e1
MinJi Cho <jam96013@gmail.com>
2025-03-25 14:10:29 +0900

    [Feat] 타이머 제거 함수 추가

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -6,13 +6,16 @@ export default function Custom404() {
   useEffect(() => {
     const timer = setTimeout(() => {
       router.push("/");
-    }, 10000);
-  });
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, [router]);
 
   return (
     <div className="flex flex-col items-center justify-center h-screen text-center">
       <h1 className="font-24b text-[var(--primary)]">404 Not Found</h1>
       <p className="mt-1 font-16r text-black3">페이지를 찾을 수 없습니다.</p>
+      <p className="mt-1 font-16r text-black3">5초 후 홈페이지로 이동합니다.</p>
     </div>
   );
 }
