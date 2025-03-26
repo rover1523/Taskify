9a18fba02cebbdd5361ec5e0df7b467511255c11
gyoyeon Kim <gy2121@naver.com>
2025-03-26 20:08:31 +0900

    [Feat] 초대받은 테이블_모바일, 태블

--- src/components/table/invited/InvitedDashBoard.tsx
+++ src/components/table/invited/InvitedDashBoard.tsx
@@ -81,10 +81,10 @@ function InvitedList({ searchTitle }: { searchTitle: string }) {
   return (
     <div className="relative bg-white w-[260px] sm:w-[504px] lg:w-[1022px] h-[770px] sm:h-[592px] lg:h-[620px] mx-auto mt-[40px]">
       {filteredData.length > 0 && (
-        <div className="p-6 flex w-full h-[26px] justify-start items-center pl-[43px] pr-[76px] md:gap-x-[130px] lg:gap-x-[280px]">
+        <div className="hidden sm:flex p-6 w-full h-[26px] justify-start items-center pl-[43px] pr-[76px] md:gap-x-[130px] lg:gap-x-[280px]">
           <p className="lg:ml-10 font-normal text-[var(--color-gray2)]">이름</p>
           <p className="font-normal text-[var(--color-gray2)]">초대자</p>
-          <p className="lg:ml-6 font-normal text-[var(--color-gray2)]">
+          <p className="lg:ml-13 font-normal text-[var(--color-gray2)]">
             수락여부
           </p>
         </div>
@@ -94,15 +94,39 @@ function InvitedList({ searchTitle }: { searchTitle: string }) {
           ? filteredData.map((invite, index) => (
               <div
                 key={index}
-                className="pb-5 mb-[20px] w-[260px] sm:w-[504px] lg:w-[1022px] h-[50px] sm:grid sm:grid-cols-[1fr_1fr_1fr] sm:items-center flex flex-col gap-2 border-b border-[var(--color-gray4)]"
+                className="pb-5 mb-[20px] w-[260px] sm:w-[504px] lg:w-[1022px] h-auto sm:h-[50px] sm:grid sm:grid-cols-[1fr_1fr_1fr] sm:items-center flex flex-col gap-10 border-b border-[var(--color-gray4)]"
               >
-                <p className="lg:ml-21 ml-9 flex justify-left mt-1 w-full">
+                {/* 모바일 레이아웃 */}
+                <div className="flex flex-col sm:hidden">
+                  <p className="ml-9 mt-1 w-full">
+                    {" "}
+                    <span className="mr-8 text-[var(--color-gray2)]">이름</span>
+                    <span className="text-[#333236]">{invite.title}</span>
+                  </p>
+                  <p className="ml-9 mt-1 w-full">
+                    <span className="mr-3.5 text-[var(--color-gray2)]">
+                      초대자
+                    </span>{" "}
+                    <span className="text-[#333236]">{invite.nickname}</span>
+                  </p>
+                  <div className="flex gap-2 mt-2 justify-center">
+                    <button className="cursor-pointer bg-[var(--primary)] text-white px-3 py-1 rounded-md w-[84px] h-[32px]">
+                      수락
+                    </button>
+                    <button className="cursor-pointer border px-3 py-1 rounded-md w-[84px] h-[32px] text-[var(--primary)] border-[var(--color-gray3)]">
+                      거절
+                    </button>
+                  </div>
+                </div>
+
+                {/* 웹, 태블릿 레이아웃 */}
+                <p className="lg:ml-21 md:ml-11 ml-9 justify-left mt-1 w-full hidden sm:flex">
                   {invite.title}
                 </p>
-                <p className="lg:ml-12 ml-9 justify-center mt-1">
+                <p className="lg:mr-25 md:mr-10 ml-9 justify-left mt-1 hidden sm:flex">
                   {invite.nickname}
                 </p>
-                <div className="lg:mr-2 flex gap-2 mt-1 justify-between sm:justify-start">
+                <div className="lg:mr-5 gap-2 mt-1 mr-2 justify-between sm:justify-start hidden sm:flex">
                   <button className="cursor-pointer bg-[var(--primary)] text-white px-3 py-1 rounded-md w-[84px] h-[32px]">
                     수락
                   </button>
@@ -115,7 +139,7 @@ function InvitedList({ searchTitle }: { searchTitle: string }) {
           : !hasMore && <NoDashboardMessage searchTitle={searchTitle} />}
 
         {filteredData.length > 0 && !hasMore && (
-          <p className="text-center text-gray-400 py-4">
+          <p className="lg:mr-18 text-center text-gray-400 py-4">
             더 이상 초대 목록이 없습니다.
           </p>
         )}
@@ -143,11 +167,11 @@ export default function InvitedDashBoard() {
   return (
     <div>
       <div className="relative bg-white rounded-lg shadow-md w-[260px] sm:w-[504px] lg:w-[1022px] h-[770px] sm:h-[592px] lg:h-[620px] max-w-none mx-auto">
-        <div className="p-6 relative w-[966px] h-[104px]">
+        <div className="p-6 relative w-full h-[104px]">
           <div className="flex justify-between items-center mb-[32px]">
             <p className="text-xl sm:text-2xl font-bold">초대받은 대시보드</p>
           </div>
-          <div className="relative w-[260px] sm:w-[448px] lg:w-[966px]">
+          <div className="relative w-[228px] sm:w-[448px] lg:w-[966px]">
             <input
               id="title"
               placeholder="검색"
