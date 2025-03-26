1ae5bb7eab0f9b7e406fb529aaa2bbf92f8e1fc3
hyejin <hhyejin90@gmail.com>
2025-03-26 21:08:29 +0900

    [Refector] 수정

--- src/pages/dashboard/[dashboardId]/index.tsx
+++ src/pages/dashboard/[dashboardId]/index.tsx
@@ -1,8 +1,17 @@
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { getColumns, getCardsByColumn } from "@/api/dashboard";
+import Column from "@/components/ColumnCard/Column";
+import { CardType, ColumnType, TasksByColumn } from "@/types/task";
+import HeaderBebridge from "@/components/Gnb/HeaderBebridge";
 
 export default function Dashboard() {
+  const [columns, setColumns] = useState<ColumnType[]>([]);
+  const [tasksByColumn, setTasksByColumn] = useState<TasksByColumn>({});
   const router = useRouter();
   const { dashboardId } = router.query;
+
+  const teamId = "13-4";
 
   const goToEditPage = () => {
     if (typeof dashboardId === "string") {
@@ -10,17 +19,51 @@ export default function Dashboard() {
     }
   };
 
+  useEffect(() => {
+    if (typeof dashboardId !== "string") return;
+
+    const fetchColumnsAndCards = async () => {
+      try {
+        // 1. 칼럼 가져오기
+        const columnRes = await getColumns({ teamId, dashboardId });
+        setColumns(columnRes.data);
+
+        // 2. 각 칼럼의 카드 가져오기
+        const columnTasks: { [columnId: number]: CardType[] } = {};
+
+        await Promise.all(
+          columnRes.data.map(async (column: ColumnType) => {
+            const cardRes = await getCardsByColumn({
+              teamId,
+              columnId: column.id,
+            });
+            columnTasks[column.id] = cardRes.cards;
+          })
+        );
+
+        setTasksByColumn(columnTasks);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    fetchColumnsAndCards();
+  }, [dashboardId]);
+
   return (
     <div>
-      <h1>대시보드 페이지 {dashboardId}</h1>
-      <br />
-      <h1>dashboardId : {dashboardId}</h1>
-      <button
-        onClick={goToEditPage}
-        className="cursor-pointer border border-[#787486] px-4 py-2 mt-4 text-blue-500 bg-white rounded-md"
-      >
-        관리 버튼
-      </button>
+      <div>
+        <HeaderBebridge />
+      </div>
+      <div className="flex gap-4 p-6">
+        {columns.map((col) => (
+          <Column
+            key={col.id}
+            title={col.title}
+            tasks={tasksByColumn[col.id] || []}
+          />
+        ))}
+      </div>
     </div>
   );
 }
