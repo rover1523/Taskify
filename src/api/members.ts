44d1fe10a4fa42d20e5350be1f6837186facf173
MinJi Cho <jam96013@gmail.com>
2025-03-27 20:34:49 +0900

    [Fix] 충돌 해결

--- src/api/members.ts
+++ src/api/members.ts
@@ -14,21 +14,8 @@ export const getMembers = async (dashboardId?: string | string[]) => {
   const dashboardIdNum =
     typeof dashboardId === "string" ? Number(dashboardId) : undefined;
 
-<<<<<<< HEAD
   const response = await axiosInstance.get<MembersResponse>(
     `https://sp-taskify-api.vercel.app/13-4/members?page=1&size=20&dashboardId=${dashboardIdNum}`
-=======
-  const response = await axios.get<ApiResponse>(
-    `${process.env.NEXT_PUBLIC_BASE_URL}13-4/members`,
-    {
-      params: {
-        dashboardId: numericDashboardId,
-      },
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    }
->>>>>>> 10ec577c7eaf3fca9342ef7c7fd37011d17b163f
   );
 
   return response.data.members || [];
