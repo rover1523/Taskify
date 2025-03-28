416b1d9c4be820037061da661697470935245c35
MinJi Cho <jam96013@gmail.com>
2025-03-28 21:37:29 +0900

    [Feat] _app.tsx 파일에 헤더 기본 출력 기능 추가, 사이드메뉴 사용 페이지에도 적용하기 위해 Layout 작성했지만 수정량이 많아 일단 폐기

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,6 @@
 import type { AppProps } from "next/app";
 import { useRouter } from "next/router";
 import HeaderDefault from "@/components/gnb/HeaderDefault";
-import HeaderDashboard from "@/components/gnb/HeaderDashboard";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -9,24 +8,16 @@ export default function App({ Component, pageProps }: AppProps) {
   const pathname = router.pathname;
 
   // 헤더 숨길 페이지
-  const noHeaderRoutes = ["/login", "/signup"];
+  const noHeaderRoutes = ["/login", "/signup", "/mydashboard", "mypage"];
   const isHeaderHidden =
     (Component as any).hideHeader || noHeaderRoutes.includes(pathname);
 
   // 상황별 헤더 분기
   const isLandingPage = pathname === "/";
-  const isMyPage = pathname === "/mypage";
-  const isMyDashboard = pathname === "/mydashboard";
-  const isDashboardPage = pathname.startsWith("/dashboard");
-  const dashboardId = router.query.dashboardId;
 
   const renderHeader = () => {
     if (isHeaderHidden) return null;
     if (isLandingPage) return <HeaderDefault variant="black" />;
-    if (isMyPage) return <HeaderDashboard variant="mypage" />;
-    if (isMyDashboard) return <HeaderDashboard variant="mydashboard" />;
-    if (isDashboardPage)
-      return <HeaderDashboard variant="dashboard" dashboardId={dashboardId} />;
     return <HeaderDefault />;
   };
 
