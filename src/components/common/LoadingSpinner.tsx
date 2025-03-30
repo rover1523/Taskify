9be4a8fe54993cdb464c930aa65852f7b3d6d97f
MinJi Cho <jam96013@gmail.com>
2025-03-31 04:53:39 +0900

    [Style] 로딩 스피너 디자인 변경

--- src/components/common/LoadingSpinner.tsx
+++ src/components/common/LoadingSpinner.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const LoadingSpinner = () => {
   return (
     <div className="flex justify-center items-center w-full h-screen bg-white">
-      <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-b-4 border-[var(--primary)]" />
+      <div className="w-12 h-12 border-6 border-[var(--primary)] border-solid rounded-full animate-spin border-t-transparent" />
     </div>
   );
 };
