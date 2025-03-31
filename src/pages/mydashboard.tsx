b7633bf126712941d4dd2952c78513ca65f5a5c9
MinJi Cho <jam96013@gmail.com>
2025-04-01 03:07:24 +0900

    [Feat] 대시보드 삭제 시 재확인 모달 오픈

--- src/pages/mydashboard.tsx
+++ src/pages/mydashboard.tsx
@@ -39,6 +39,8 @@ export default function MyDashboardPage() {
   >(null);
   const [selectedTitle, setSelectedTitle] = useState<string | null>(null);
   const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+  const [isConfirmDeleteModalOpen, setIsConfirmDeleteModalOpen] =
+    useState(false);
   const itemsPerPage = 6;
 
   const totalPages = Math.ceil((dashboardList.length + 1) / itemsPerPage);
@@ -185,13 +187,13 @@ export default function MyDashboardPage() {
       >
         <div className="flex flex-col items-center gap-1 text-center">
           <div className="text-[var(--primary)] font-16m">{selectedTitle}</div>
-
           <div className="text-black3 font-16m">
             {selectedCreatedByMe
               ? "대시보드를 삭제하시겠습니까?"
               : "대시보드에서 나가시겠습니까?"}
           </div>
         </div>
+
         <div className="flex items-center justify-center gap-2">
           <CustomBtn
             onClick={() => setIsDeleteModalOpen(false)}
@@ -200,9 +202,47 @@ export default function MyDashboardPage() {
             취소
           </CustomBtn>
           <CustomBtn
-            onClick={
-              selectedCreatedByMe ? () => handleDelete() : () => handleLeave()
-            }
+            onClick={() => {
+              if (selectedCreatedByMe) {
+                setIsDeleteModalOpen(false);
+                setIsConfirmDeleteModalOpen(true); // 재확인 모달 오픈
+              } else {
+                handleLeave(); // 탈퇴일 때는 바로 닫힘
+              }
+            }}
+            className="cursor-pointer bg-[var(--primary)] text-white px-3 py-1 rounded-md w-[84px] h-[32px]"
+          >
+            확인
+          </CustomBtn>
+        </div>
+      </Modal>
+
+      <Modal
+        width="w-[260px]"
+        height="h-[150px]"
+        isOpen={isConfirmDeleteModalOpen}
+        onClose={() => setIsConfirmDeleteModalOpen(false)}
+        className="flex items-center justify-center text-center"
+      >
+        <div className="flex flex-col items-center gap-1 text-center">
+          <div className="text-red-400 font-16m">
+            삭제 시 복구할 수 없습니다.
+          </div>
+          <div>정말 삭제하시겠습니까?</div>
+        </div>
+
+        <div className="flex items-center justify-center gap-2">
+          <CustomBtn
+            onClick={() => setIsConfirmDeleteModalOpen(false)}
+            className="cursor-pointer border px-3 py-1 rounded-md w-[84px] h-[32px] text-[var(--primary)] border-[var(--color-gray3)]"
+          >
+            취소
+          </CustomBtn>
+          <CustomBtn
+            onClick={() => {
+              setIsConfirmDeleteModalOpen(false);
+              handleDelete(); // 진짜 삭제 실행
+            }}
             className="cursor-pointer bg-[var(--primary)] text-white px-3 py-1 rounded-md w-[84px] h-[32px]"
           >
             확인
