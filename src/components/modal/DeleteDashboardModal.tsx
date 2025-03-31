ed655ae9b0b59d74bd83194f22fd959812868661
gyoyeon Kim <gy2121@naver.com>
2025-04-01 02:25:36 +0900

    [Style] 대시보드 삭제 모달 반응형 수정

--- src/components/modal/DeleteDashboardModal.tsx
+++ src/components/modal/DeleteDashboardModal.tsx
@@ -39,7 +39,7 @@ export default function DeleteDashboardModal({
       height="h-[160px] sm:h-[174px]"
     >
       <div className="flex flex-col sm:gap-10 gap-6 text-center">
-        <p className="text-xl mt-1.5">대시보드를 정말 삭제하시겠습니까?</p>
+        <p className="text-xl mt-1.5">대시보드를 삭제하시겠습니까?</p>
         <div className="flex justify-between gap-3">
           <CustomBtn variant="outlineDisabled" onClick={onClose}>
             취소
