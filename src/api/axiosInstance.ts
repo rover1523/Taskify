e898639d051133d4c57cad47ccf3dcff3af4fb7b
hyejin <hhyejin90@gmail.com>
2025-04-02 13:35:19 +0900

    [Feat] 카드 생성 후 새로고침 기능 추가

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,7 +1,18 @@
+// axiosInstance.ts
+
 import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: "https://sp-taskify-api.vercel.app",
+});
+
+// 👉 Authorization 헤더 자동 설정, 요청 보낼때 마다 localstorage에서 토큰 가져오기
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("accessToken"); // localStorage에서 토큰 가져오기
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`; // 헤더에 Authorization 추가
+  }
+  return config;
 });
 
 // 👉 요청 보낼 때마다 토큰 자동 추가
